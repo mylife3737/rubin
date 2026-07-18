e669d06d4b88ec6eb129cb30f20f41e68b591068
Diana <isthismylife37@gmail.com>
2026-07-18 17:47:27 -0400

    trim: drop portfolio scaffolding and demo sites; keep only FixItFirst

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,5 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './hooks/useTheme';
-import Home from './pages/Home';
-import ProjectDetail from './pages/ProjectDetail';
-import FixitSam from './pages/FixitSam';
-import UpdatedPolicies from './pages/UpdatedPolicies';
+import FixItFirst from './pages/FixitSam';
 
 export default function App() {
-  return (
-    <ThemeProvider>
-      <Router basename={import.meta.env.BASE_URL}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project/:projectId" element={<ProjectDetail />} />
-          <Route path="/fix-it-first" element={<FixitSam />} />
-          <Route path="/fixit-sam" element={<FixitSam />} />
-          <Route path="/updated-policies" element={<UpdatedPolicies />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
-  );
+  return <FixItFirst />;
 }
